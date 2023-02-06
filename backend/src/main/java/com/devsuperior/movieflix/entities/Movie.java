8ff9c331923bdb5c2015b04aff30934be07a7bf4
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-02-06 15:57:13 -0300

    Annotation Column in synopsis

--- backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -21,6 +21,8 @@ public class Movie implements Serializable {
     private String subTitle;
     private Integer year;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String synopsis;
 
     @ManyToOne
